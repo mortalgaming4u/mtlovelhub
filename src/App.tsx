f291d8685fc29cb8bd422fe120895600b685f6f7
mortalgaming4u <mortalgaming4u@gmail.com>
2025-08-29 01:02:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react"; // ✅ Required for JSX
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import NovelList from "./pages/NovelList";
@@ -18,33 +16,29 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/novel-list" element={<NovelList />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/ranking/:type" element={<Ranking />} />
-              <Route path="/ranking" element={<Ranking />} />
-              <Route path="/random" element={<Home />} />
-              <Route path="/recommendations" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/profile/*" element={<Profile />} />
-              {/* ✅ NEW ROUTE */}
-              <Route path="/request" element={<RequestPage />} />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/novel-list" element={<NovelList />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/ranking/:type" element={<Ranking />} />
+            <Route path="/ranking" element={<Ranking />} />
+            <Route path="/random" element={<Home />} />
+            <Route path="/recommendations" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/*" element={<Profile />} />
+            {/* ✅ NEW ROUTE */}
+            <Route path="/request" element={<RequestPage />} />
+            {/* Catch-all route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 
