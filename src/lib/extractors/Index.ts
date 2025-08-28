37848e19bb3f8ccfb1d2d93cfa1f6314b14863c3
mortalgaming4u <mortalgaming4u@gmail.com>
2025-08-28 19:45:47 +0530

    Update Index.ts

--- src/lib/extractors/Index.ts
+++ src/lib/extractors/Index.ts
@@ -1,1 +1,11 @@
-w
+import { extractTwkan } from './twkan';
+import { extractPiaotia } from './piaotia';
+
+export async function scrapeBook(url: string) {
+  const domain = new URL(url).hostname;
+
+  if (domain.includes('twkan.com')) return await extractTwkan(url);
+  if (domain.includes('piaotia.com')) return await extractPiaotia(url);
+
+  throw new Error(`Unsupported domain: ${domain}`);
+}
